8bf10fb2021aca94290a525b3a2c73169a19637a
KMirecki <k.mirecki13@wp.pl>
2024-05-27 22:41:58 +0200

    changes

--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
-def add(a: int, b:int)->int:
-    return a+b
+"""4 functions performing basic math operations"""
 
-def substract (a: int, b: int) -> int:
-    return a-b
 
-def multiply(a: int, b:int) -> int:
-    return a*b
+def add(a: int, b: int) -> int:
+    """function adds two integers"""
+    return a + b
 
-def divide(a:int, b:int) -> float:
-    return a/b
+
+def substract(a: int, b: int) -> int:
+    """function substracts two integers"""
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """function multiplies two integers"""
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """function divides two integers and returns float"""
+    return a / b
